cf418388d841196df5dcfde213e00daf5915ed11
Morialle Barr <Moriallekbarr@gmail.com>
2025-06-29 12:15:47 +0300

    Lessons 4-6

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,20 +2,17 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    float xValue = 0f;
-    float yValue = 0.05f;
-    float zValue = 0f;
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float xValue = Input.GetAxis("Horizontal");
+         float yValue = 0f;
+         float zValue = Input.GetAxis("Vertical");
         transform.Translate(xValue, yValue, zValue);
     }
 }
